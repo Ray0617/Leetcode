e6cae8de5cbec65432e04243a75df930d012a543
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2015-01-22 00:12:19 -0500

    add Balanced Binary Tree
    Change-Id: I52d7e1228ebef0ef71e5a0461d384fbc08b11349

---
+++ Balanced Binary Tree.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int height(TreeNode node) {
+        if (node == null)
+            return 0;
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+}
---
+++ Balanced Binary Tree.py
@@ -0,0 +1,18 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def height(self, root):
+        if (root == None):
+            return 0
+        return max(self.height(root.left), self.height(root.right)) + 1
+    # @param root, a tree node
+    # @return a boolean
+    def isBalanced(self, root):
+        if (root == None):
+            return True
+        return self.isBalanced(root.left) and self.isBalanced(root.right) and abs(self.height(root.left)-self.height(root.right)) <= 1
